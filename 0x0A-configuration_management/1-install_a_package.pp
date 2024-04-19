14db6fcaa13b3af5e232ef2bf52c4f61923e824b
devarim <wachirabriangeita@gmail.com>
2024-04-19 14:23:58 +0300

    allow

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
+# 1-install_a_package.pp
+# Puppet manifest to install Flask version 2.1.0 using pip3
 
-exec { 'install Flask':
+exec { 'install Flask 2.1.0':
   command => 'pip3 install Flask==2.1.0',
   unless  => 'pip3 freeze | grep Flask==2.1.0',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
