b8c9e93a5ef3db7f2963c89e6be231d9f5db4602
devarim <wachirabriangeita@gmail.com>
2024-06-04 20:30:10 +0300

    API Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,15 +7,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts listed for a given subreddit."""
+    """Print the titles of the first
+       10 hot posts listed for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         for post in data['data']['children']:
             print(post['data']['title'])
     else:
         print("None")
-
